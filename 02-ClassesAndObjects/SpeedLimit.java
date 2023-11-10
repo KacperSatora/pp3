50c58debf698e50567803520732a5a37741ae86c
Kacper Satora <kacpersatora02@gmail.com>
2023-11-10 20:19:16 +0100

    02 update

---
+++ 02-ClassesAndObjects/SpeedLimit.java
@@ -0,0 +1,10 @@
+public class SpeedLimit {
+    public static void main(String[] args) {
+        int speeed = 140;
+        if (speeed >= 40 && speeed <= 140) {
+            System.out.println("Speed is valid");
+        } else {
+            System.out.println("Speed is not valid");
+        }
+    }
+}
